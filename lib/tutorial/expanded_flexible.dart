b92f2e4cefb9fc6f0959772cdc6e80efce37897d
Adi Munawar <munawar.adi31@gmail.com>
2022-03-06 20:47:32 +0700

    Expanded and Flexible

---
+++ lib/tutorial/expanded_flexible.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class ExpandedFlexiblePage extends StatelessWidget {
+  const ExpandedFlexiblePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ExpandedWidget(),
+                FlexibleWidget(),
+              ],
+            ),
+            Row(
+              children: [
+                ExpandedWidget(),
+                ExpandedWidget(),
+              ],
+            ),
+            Row(
+              children: [
+                FlexibleWidget(),
+                FlexibleWidget(),
+              ],
+            ),
+            Row(
+              children: [
+                FlexibleWidget(),
+                ExpandedWidget(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExpandedWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.teal,
+          border: Border.all(color: Colors.white),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Expanded',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FlexibleWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.tealAccent,
+          border: Border.all(color: Colors.white),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text(
+            'Flexible',
+            style: TextStyle(
+              color: Colors.teal,
+              fontSize: 24,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
