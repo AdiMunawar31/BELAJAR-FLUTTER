d9a96b1cd1cc9259abec11701bd57bf81550ba5f
Adi Munawar <munawar.adi31@gmail.com>
2022-03-16 20:09:22 +0700

    navigation

---
+++ lib/tutorial/navigation.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  final String message = 'Hello from First Screen!';
+
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Pindah Screen'),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SecondScreen(message)));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  final String message;
+
+  // ignore: use_key_in_widget_constructors
+  const SecondScreen(this.message);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(message),
+            OutlinedButton(
+              child: Text('Kembali'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
